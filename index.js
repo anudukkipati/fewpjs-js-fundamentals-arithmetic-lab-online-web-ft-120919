40eed7b2164129150c126d5e8a9e5695365d5fc2
anupama <anupamagadde@yahoo.com>
2020-04-02 16:41:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply;
+let random;
+let mod;
+let max;
+
+let num1 = 2;
+let num2 = 31;
+multiply = num1 * num2;
+
+random = Math.floor(Math.random() * 10) + 1;
+
+mod = 64 % 10;
+
+max = Math.max(12, 6, 1, 10, 4, 20);
